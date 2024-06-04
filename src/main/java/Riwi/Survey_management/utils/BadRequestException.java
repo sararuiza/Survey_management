773006cdeff9553bcb306b3a882c34ca3003dba5
sararuiza <149072353+sararuiza@users.noreply.github.com>
2024-06-04 10:04:06 -0500

    Revert "Feature/task dev 02"

--- src/main/java/Riwi/Survey_management/utils/BadRequestException.java
+++
@@ -1,9 +0,0 @@
-package Riwi.Survey_management.utils;
-
-public class BadRequestException extends RuntimeException {
-
-    public BadRequestException(String message){
-        super(message);
-    }
-    
-}
