2c310f002929f921e2fba7f097c96ecac8919e6f
sararuiza <149072353+sararuiza@users.noreply.github.com>
2024-06-04 06:38:14 -0500

    init proyect

---
+++ src/main/java/Riwi/Survey_management/SurveyManagementApplication.java
@@ -0,0 +1,13 @@
+package Riwi.Survey_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurveyManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SurveyManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Riwi/Survey_management/SurveyManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Riwi.Survey_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
